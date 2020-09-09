a8dd2d2bb001b426805719d757ea147d548ff488
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-08 19:22:05 -0600

    Add test to user_spec.rb to display the flash message saying they are missing fields

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe "User Registration", type: :feature do
       end
 
       expect(current_path).to eq('/register')
-      save_and_open_page
 
       fill_in :name, with: "Captain Jack"
       fill_in :street_address, with: "123 Main Street"
@@ -25,6 +24,30 @@ RSpec.describe "User Registration", type: :feature do
       click_on "submit"
       expect(current_path).to eq('/profile')
       expect(page).to have_content('You are now registered and logged in!')
+    end
+
+    it "can display error message" do
+      visit '/items'
+
+      within '.topnav' do
+        click_link "Register"
+      end
+
+      expect(current_path).to eq('/register')
+
+      fill_in :name, with: "Captain Jack"
+      fill_in :street_address, with: "123 Main Street"
+      fill_in :city, with: "Denver"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "11111"
+      fill_in :email, with: ""
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+
+      click_on "submit"
+      save_and_open_page
+      expect(current_path).to eq('/register')
+      expect(page).to have_content("You are missing fields, please fill in all fields to register!")
     end
   end
 end
