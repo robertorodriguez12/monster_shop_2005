11b7082df64a2c63c9aea1a630249e104f8d1223
Austin Aspaas <araspaas@live.com>
2020-09-09 18:33:47 -0600

    Create sessions controller with new and create actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,13 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
 
   def new
-    
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
+    flash[:success] = "Welcome #{user.email}"
+    redirect_to '/profile' #need to update later for different users and correct path
   end
 end
