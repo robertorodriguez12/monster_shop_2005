c4aff2f72f32a919ad917a07873c5565156920dc
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-12 11:44:23 -0600

    Add model test for five least popular items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -82,5 +82,35 @@ describe Item, type: :model do
       expect(Item.top_five_items).to eq(expected)
 
     end
+
+    it 'bottom five items' do
+      chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      #dog_shop items
+      pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      dog_bone = @dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
+      lizard_head = @dog_shop.items.create(name: "Chew Toy", description: "Yummy", price: 500, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
+      barbie = @dog_shop.items.create(name: "She needs a friend", description: "Very Pink", price: 1000, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:true, inventory: 21)
+      order = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_1 = Order.create(name: 'Roberto', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_2 = Order.create(name: 'Brett', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_3 = Order.create(name: 'Nico', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+
+
+      order.item_orders.create(item: tire, price: tire.price, quantity: 2)
+      order.item_orders.create(item: pull_toy, price: pull_toy.price, quantity: 5)
+      order.item_orders.create(item: dog_bone, price: tire.price, quantity: 7)
+      order.item_orders.create(item: lizard_head, price: lizard_head.price, quantity: 8)
+      order.item_orders.create(item: barbie, price: barbie.price, quantity: 9)
+      order.item_orders.create(item: chain, price: chain.price, quantity: 6)
+
+      # binding.pry
+
+      expected = [tire, pull_toy, chain, dog_bone, lizard_head]
+
+
+      expect(Item.five_least_popular_items).to eq(expected)
+
+    end
   end
 end
