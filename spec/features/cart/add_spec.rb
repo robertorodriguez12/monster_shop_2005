2cb9d80cba124105780e8a4f93ddefe535f1c52f
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-12 14:00:38 -0600

    add tests for subtract and add quantity

--- spec/features/cart/add_spec.rb
+++ spec/features/cart/add_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Cart creation' do
     before(:each) do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
-      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 5)
     end
 
     it "I see a link to add this item to my cart" do
@@ -30,6 +30,48 @@ RSpec.describe 'Cart creation' do
 
       within 'nav' do
         expect(page).to have_content("Cart: 2")
+      end
+    end
+
+      it "can add item quantity to the cart" do
+        visit "/items/#{@pencil.id}"
+        click_on "Add To Cart"
+        visit '/cart'
+
+        within "#cart-item-#{@pencil.id}" do
+          expect(page).to have_link('+')
+          expect(page).to have_link('-')
+          click_on '+'
+          click_on '+'
+          click_on '+'
+          click_on '+'
+          click_on '+'
+          expect(page).to have_content(5)
+        end
+    end
+    it "can subtract item quantity in the cart" do
+      visit "/items/#{@pencil.id}"
+      click_on "Add To Cart"
+      visit '/cart'
+
+      within "#cart-item-#{@pencil.id}" do
+        expect(page).to have_link('+')
+        expect(page).to have_link('-')
+        click_on '+'
+        click_on '+'
+        click_on '+'
+        click_on '+'
+        click_on '+'
+        expect(page).to have_content(5)
+      end
+
+      within "#cart-item-#{@pencil.id}" do
+        expect(page).to have_link('+')
+        expect(page).to have_link('-')
+        click_on '-'
+        click_on '-'
+        click_on '-'
+        expect(page).to have_content(2)
       end
     end
   end
