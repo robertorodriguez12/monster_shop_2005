824903601cf82dfaa5de6aa8abe72d782f9341f0
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-11 19:24:06 -0600

    Completes us:14 cannot login with bad credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,25 @@
 class SessionsController < ApplicationController
 
   def new
+    # if session[:user_id] != nil
+    #   flash[:success] = "You're already logged in"
+    #   redirect_user
+    # end
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
-    flash[:success] = "Welcome #{user.email}"
-    redirect_user #need to update later for different users and correct path
+      user = User.find_by(email: params[:email])
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        flash[:success] = "Welcome, #{user.name}!"
+        redirect_user
+      else
+        flash[:error] = "Sorry, your credentials are bad."
+        render :new
+      end
   end
 
   def redirect_user
-      #binding.pry
       if current_user.role == "regular_user"
         redirect_to '/profile'
       elsif merchant?
