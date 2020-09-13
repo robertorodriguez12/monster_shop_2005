496bf9a537aad103adeaaaaae175650721915c08
Nico Rithner <nicorithner@gmail.com>
2020-09-13 14:33:43 -0600

    Update test

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'item delete', type: :feature do
+  before :each do
+    @user = User.create(email: "funbucket13@gmail.com", password: "test", name: "Mike Dao", street_address: "123 easy street", city: "Anycity", state: "Anystate", zip: 12345, role: 0)
+  end
   describe 'when I visit an item show page' do
     it 'I can delete an item' do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -31,7 +34,7 @@ RSpec.describe 'item delete', type: :feature do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
       review_1 = chain.reviews.create(title: "Great place!", content: "They have great bike stuff and I'd recommend them to anyone.", rating: 5)
-      order_1 = Order.create!(name: 'Meg', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80218)
+      order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80218)
       order_1.item_orders.create!(item: chain, price: chain.price, quantity: 2)
 
       visit "/items/#{chain.id}"
