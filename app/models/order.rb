a2e698f340409f969e517ac76b18e0e0e13733e5
Nico Rithner <nicorithner@gmail.com>
2020-09-13 12:47:14 -0600

    Add enum status

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,20 @@ class Order <ApplicationRecord
 
   has_many :item_orders
   has_many :items, through: :item_orders
+  belongs_to :user
+
+  enum status: [ :pending, :packaged, :shipped, :cancelled ]
 
   def grandtotal
     item_orders.sum('price * quantity')
   end
+  def total_items
+    # binding.pry
+    # items.select('items.name').where('item_orders.item_id = items.id')
+
+    # items.select('items.name', 'items.id','count(items.id) AS total').where('item_orders.item_id = items.id').group('items.id').order(total: :desc)
+  end
+  # def total_items
+  #   item_orders.sum('quantity')
+  # end
 end
