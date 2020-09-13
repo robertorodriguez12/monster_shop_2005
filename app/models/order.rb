c3cf0f125c411b85d2f92b3e8e7f35f597bf33c0
Nico Rithner <nicorithner@gmail.com>
2020-09-13 14:35:24 -0600

    Create class method total_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,22 +1,16 @@
 class Order <ApplicationRecord
-  validates_presence_of :name, :address, :city, :state, :zip
-
+  belongs_to :user
+  
   has_many :item_orders
   has_many :items, through: :item_orders
-  belongs_to :user
-
+  validates_presence_of :name, :address, :city, :state, :zip
+  
   enum status: [ :pending, :packaged, :shipped, :cancelled ]
 
   def grandtotal
     item_orders.sum('price * quantity')
   end
   def total_items
-    # binding.pry
-    # items.select('items.name').where('item_orders.item_id = items.id')
-
-    # items.select('items.name', 'items.id','count(items.id) AS total').where('item_orders.item_id = items.id').group('items.id').order(total: :desc)
+    item_orders.sum(:quantity)
   end
-  # def total_items
-  #   item_orders.sum('quantity')
-  # end
 end
