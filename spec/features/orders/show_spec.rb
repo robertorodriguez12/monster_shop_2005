f3cbd91611ffc3af3cefee32ce8d93cd1b08b75a
Nico Rithner <nicorithner@gmail.com>
2020-09-16 15:50:13 -0600

    added spaces between mechants products and user in the test set up"

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe("Order show Page") do
     before(:each) do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      
       @user = User.create(email: "c_j@email.com", password: "test", name: "Mike Dao", city: "blah", state: "blah", street_address: "blah", zip: 12345, role: 0)
+      
       @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
 
       @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
@@ -59,6 +62,11 @@ RSpec.describe("Order show Page") do
       within("#unfulfilled-#{@pencil.id}") do
         expect(page).to have_content("Fulfillment Status: #{@pencil.item_orders.first.status}")
       end
+
+      within("#unfulfilled-#{@pencil.id}") do
+        expect(page).to have_content("Fulfillment Status: #{@pencil.item_orders.first.status}")
+      end
+
     end
   end
 end
