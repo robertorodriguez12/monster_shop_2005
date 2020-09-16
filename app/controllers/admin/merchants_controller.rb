e0d459e50026905c9ec2542cffc001e92b5f7f63
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-16 13:37:50 -0600

    Create a method in admin/merchants controller to disable a merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -9,4 +9,11 @@ class Admin::MerchantsController < Admin::BaseController
     @merchant = Merchant.find(params[:id])
   end
 
+  def disable_merchant
+    @merchant = Merchant.find(params[:id])
+    @merchant.disable
+    flash[:success] = "This merchant has been disabled."
+    redirect_to '/admin/merchants'
+  end
+
 end
