325d2dfb89c667245972f08845f397013f193565
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-09-17 14:24:57 -0600

    remove unecessary method

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,10 +4,4 @@ class Admin::DashboardController < Admin::BaseController
     @orders = Order.all
   end
 
-  def show
-    if current_user.role == "user_admin"
-      @admin = Admin.find(current_user.admin_id)
-    end
-  end
-
 end
